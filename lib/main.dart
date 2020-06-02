fe20ab0d3d25723a146c561fcac53d727e0ca886
Camposlito <henrique_kuster14@hotmail.com>
2020-06-02 15:03:02 -0300

    Change icon

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Todo App',
+      title: 'To do App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
