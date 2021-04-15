e2a57d8930572456e3cec6ba1f9a7b6ce4e64634
Henrique Campos <henrique_kuster14@hotmail.com>
2021-04-14 21:49:42 -0300

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'models/item.dart';
-
+//
 void main() {
   runApp(MyApp());
 }
